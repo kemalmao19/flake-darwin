e9cc45a9326930b6b05185fb7566a8438d37aa08
kemal maolana <kemalmao@192.168.1.4>
2023-09-03 00:21:16 +0700

    fish abbrvs

--- modules/home-manager/packages/fish.nix
+++ modules/home-manager/packages/fish.nix
@@ -4,6 +4,24 @@
     shellAbbrs = {
       cp = "cp -iv";
       mv = "mv -iv";
+      rm = "trash-put";
+      cat = "bat";
+      less = "less -i";
+      gst = "git status";
+      gco = "git checkout";
+      gcm = "git commit -m";
+      gai = "git add -i";
+      gpull = "git pull";
+      gpush = "git push";
+      py = "python";
+      startdb = "systemctl start mysql";
+      stopdb = "systemctl stop mysql";
+      statdb = "systemctl status mysql";
+      nixpkg = "nix-env -iA nixpkgs";
+      nixospkg = "nix-env -iA nixos";
+      # vi = "hx";
+      # ls = "exa";
+      # top = "btm";
     };
   };
 }
